CyanogenMod/android_device_motorola_msm8226-common

msm8226-common: Update rild services for lollipop
Second SIM hooks to rild2 socket in Lollipop.

Change-Id: I7e59eb06f1ca74d7eaa2f18f650ef1d5f9b4cdd9

include/device_perms.h
@@ -34,7 +34,6 @@
 
 #define CONTROL_PERMS_APPEND \
     { "mmi-ril-daemon",AID_RADIO, AID_RADIO }, \
-    { "ril-daemon1",AID_RADIO, AID_RADIO }, \
-    { "ril-daemon2",AID_RADIO, AID_RADIO },
+    { "mmi-ril-daemon2",AID_RADIO, AID_RADIO },
 
 #endif /* DEVICE_PERMS_H */

sub77-du/android_device_motorola_msm8226

include/device_perms.h
@@ -34,7 +34,6 @@
 
 #define CONTROL_PERMS_APPEND \
     { "mmi-ril-daemon",AID_RADIO, AID_RADIO }, \
-    { "ril-daemon1",AID_RADIO, AID_RADIO }, \
-    { "ril-daemon2",AID_RADIO, AID_RADIO },
+    { "mmi-ril-daemon1",AID_RADIO, AID_RADIO },
 
 #endif /* DEVICE_PERMS_H */